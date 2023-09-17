bdd25b278844ea6e1c855dbf20b8eb9b3aade40d
heathitlabs <jason.heath@heathitlabs.com>
2023-09-16 19:27:56 -0500

    added ui elements to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,9 @@ import Link from "next/link"
 import { Poppins } from "next/font/google"
 
 import { cn } from "@/lib/utils"
+import { UserButton } from "@clerk/nextjs"
+import { Button } from "./ui/button"
+import { Sparkles } from "@/node_modules/lucide-react"
 
 const font = Poppins({
     weight: "600",
@@ -16,9 +19,9 @@ export const Navbar = () => {
         <div className="fixed w-full z-50 flex justify-between
              items-center py-2 px-4 border-b border-primary/10 bg-secondary">
             <div className="flex items-center">
-                <Menu className="block md-hidden"/>
+                <Menu className="block md:hidden"/>
                 <Link href="/">
-                    <h1 className={cn(
+                 <h1 className={cn(
                         "hidden md:block text-xl md:text-3xl font-bold text-primary",
                         font.className
                     )}>
@@ -26,7 +29,14 @@ export const Navbar = () => {
                     </h1>
                 </Link>
             </div>
+            <div className="flex items-center gap-x-3">
+                <Button >
+                    Upgrade
+                     <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
+                </Button>
+                <UserButton />
+            </div>
         </div>
      );
 }
- 
+
