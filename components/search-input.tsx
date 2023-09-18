4d508bb808b8db9f60093d0ee4993836b556043b
heathitlabs <jason.heath@heathitlabs.com>
2023-09-18 10:52:31 -0500

    added searching functionality

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,9 +1,11 @@
 "use client"
 
-import { useState } from "react"
+import { ChangeEventHandler, useEffect, useState } from "react"
 import { Search } from "lucide-react"
 import { useRouter, useSearchParams } from "next/navigation"
 import { Input } from "@/components/ui/input"
+import { useDebounce } from "@/hooks/use-debounce"
+import qs from "query-string"
 
 export const SearchInput = () => {
     const router = useRouter()
@@ -13,11 +15,32 @@ export const SearchInput = () => {
     const name = searchParams.get("name")
 
     const [value, setValue] = useState(name || "")
+    const debouncedValue = useDebounce<string>(value, 500)
+
+    const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setValue(e.target.value)
+    }
+
+    useEffect(() => {
+        const query = {
+            name: debouncedValue,
+            categoryId: categoryId
+        }
+
+        const url = qs.stringifyUrl({
+            url: window.location.href,
+            query
+        }, { skipEmptyString: true, skipNull: true })
+
+        router.push(url)
+    }, [debouncedValue, router, categoryId])
 
     return(
         <div className="relative">
            <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
-           <Input 
+           <Input
+            onChange={onChange}
+            value={value}
             placeholder="Search..."
             className="pl-10 bg-primary/10"
            />
--- hooks/use-debounce.ts
+++ hooks/use-debounce.ts
@@ -6,6 +6,11 @@ export function useDebounce<T>(value: T, delay?: number): T {
     useEffect(() =>{
         const timer = setTimeout(() => setDebouncedValue(value),
         delay || 500)
-        
+    
+        return () => {
+            clearTimeout(timer)
+        }
     },[value,delay])
+
+    return debouncedValue
 }
